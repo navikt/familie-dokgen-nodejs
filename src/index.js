7e4fe15a046c231339699670c65cca9a43e7db39
Peter Rydberg <hmp.rydberg@gmail.com>
2019-06-27 13:37:17 +0200

    Added request debugging for dev

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyparser from 'body-parser';
+import morgan from 'morgan';
 import { port } from '../config';
 import routes from './routes';
 
@@ -8,6 +9,7 @@ const server_port = port || 3000;
 
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: false }));
+app.use(morgan('dev'));
 routes(app);
 
 app.listen(server_port, () => console.log(`Server listening on port ${server_port}.`));
