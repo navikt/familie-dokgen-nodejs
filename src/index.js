f889f041389b513292256bae0777380b3305f3da
Peter Rydberg <hmp.rydberg@gmail.com>
2019-06-27 09:31:10 +0200

    Added body parser for post requests

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import express from 'express';
+import bodyparser from 'body-parser';
 import { port } from '../config';
 import routes from './routes';
 
 const app = express();
 const server_port = port || 3000;
 
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: false }));
 routes(app);
 
 app.listen(server_port, () => console.log(`Server listening on port ${server_port}.`));
