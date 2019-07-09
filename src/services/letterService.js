93fa4ff554a333e640953550abda137e5946b933
Peter Rydberg <hmp.rydberg@gmail.com>
2019-07-09 08:46:11 +0200

    Added JSON verification

--- src/services/letterService.js
+++ src/services/letterService.js
@@ -1,21 +1,73 @@
 import handlebars from "handlebars";
-//import markdown from 'handlebars-helpers';
+import fs from "mz/fs";
+import path from "path";
 import templateService from "./templateService";
 import showdown from 'showdown';
+import Ajv from 'ajv';
 
 export default {
 
     async createLetter(templateName, interleavingFields = {}){
         const markdownTemplate = await templateService.findMarkdownTemplate(templateName);
-        const converter = new showdown.Converter();
-        const html = converter.makeHtml(markdownTemplate);
-        //handlebars.registerHelper("markdown", helpers.markdown({handlebars: handlebars}));
 
+        try {
+            return await this.processJson(templateName, interleavingFields)
+                .then( (jsonData) => {
+                    const hbs = handlebars.compile(markdownTemplate);
+                    const compiledHbs = hbs(jsonData);
+                    const converter = new showdown.Converter();
+                    return converter.makeHtml(compiledHbs);
+                })
+                .catch((error) => {
+                    throw new Error(error.message)
+                });
+        }
+        catch (error) {
+            throw new Error(error.message);
+        }
+    },
+
+    async processJson(templateName, interleavingFields){
         interleavingFields = typeof interleavingFields !== "string"
             ? JSON.stringify(interleavingFields) : interleavingFields;
         const jsonData = JSON.parse(interleavingFields);
 
-        const hbs = handlebars.compile(html);
-        return hbs(jsonData);
-    }
+        try {
+            return await this.findJsonSchema(templateName)
+                .then((schema) => {
+                    const validationSchema = JSON.parse(schema);
+                    return this.validateJson(validationSchema, jsonData);
+                })
+                .catch((error) => {
+                    throw new Error(error.message)
+                });
+        }
+        catch (error) {
+            throw new Error(error.message)
+        }
+    },
+
+    validateJson(schema, jsonData){
+        const ajv = new Ajv({allErrors: true});
+        const validator = ajv.compile(schema);
+        const valid = validator(jsonData);
+        return valid ? jsonData : console.log(validator.errors);
+    },
+
+    getJsonSchemaPath(templateName){
+        return path.join(__dirname + `/../templates/${templateName}/${templateName}.json`);
+    },
+
+    async findJsonSchema(templateName){
+        const path = this.getJsonSchemaPath(templateName);
+        return await fs.readFile(path, 'utf-8')
+            .then(data => {
+                return data;
+            })
+            .catch((err) => {
+                if(err.code === "ENOENT"){
+                    throw new Error("Could not find JSON schema to validate on.")
+                }
+            });
+    },
 }
