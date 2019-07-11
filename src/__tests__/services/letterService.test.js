b7a0124e7306bae3ac67b83ca73aacfb7bf36090
Peter Rydberg <hmp.rydberg@gmail.com>
2019-07-11 09:07:44 +0200

    Moved json validation to standalone utils file

--- src/__tests__/services/letterService.test.js
+++ src/__tests__/services/letterService.test.js
@@ -1,6 +1,7 @@
 import sinon from "sinon";
 import mockFs from "mock-fs";
 import { expect } from "chai";
+import jsonValidation from "../../utils/jsonValidation";
 import letterService from "../../services/letterService";
 import templateService from "../../services/templateService";
 import InterleavingFieldsError from "../../utils/Exceptions/InterleavingFieldsError";
@@ -20,13 +21,13 @@ describe('When using letter service,', () => {
 
     beforeEach(() => {
         sinon.stub(templateService, "getMarkdownTemplatePath").callsFake(getMarkdownTemplatePath);
-        sinon.stub(letterService, "getJsonSchemaPath").callsFake(getJsonSchemaPath);
+        sinon.stub(jsonValidation, "getJsonSchemaPath").callsFake(getJsonSchemaPath);
         mockFs(dir1, {createCwd: true, createTmp: true});
     });
 
     afterEach(() => {
         templateService.getMarkdownTemplatePath.restore();
-        letterService.getJsonSchemaPath.restore();
+        jsonValidation.getJsonSchemaPath.restore();
         mockFs.restore();
     });
 
@@ -56,7 +57,7 @@ describe('When using letter service,', () => {
     describe('processing JSON', () => {
 
         it('should return the JSON data when input structure is ok', () => {
-            return letterService.processJson('tem1', interleavingFields1)
+            return jsonValidation.processJson('tem1', interleavingFields1)
                 .then((data) => {
                     expect(data.constructor === {}.constructor).to.be.true;
                     expect(JSON.stringify(data)).to.be.equal(JSON.stringify(interleavingFields1));
@@ -64,7 +65,7 @@ describe('When using letter service,', () => {
         });
 
         it('should return an error when the input structure is not ok', () => {
-            return letterService.processJson('tem1', interleavingFields2)
+            return jsonValidation.processJson('tem1', interleavingFields2)
                 .catch((error) => {
                     expect(error).to.exist;
                     expect(error instanceof InterleavingFieldsError).to.be.true;
@@ -75,7 +76,7 @@ describe('When using letter service,', () => {
         });
 
         it('should return the JSON data when verify is set to false', () => {
-            return letterService.processJson('tem1', interleavingFields1, false, "html")
+            return jsonValidation.processJson('tem1', interleavingFields1, false, "html")
                 .then((data) => {
                     expect(data.constructor === {}.constructor).to.be.true;
                     expect(JSON.stringify(data)).to.be.equal(JSON.stringify(interleavingFields1));
@@ -88,14 +89,14 @@ describe('When using letter service,', () => {
         it('should return the JSON data when input structure is ok', () => {
             const valJson = JSON.stringify(schema1);
             const testJson = JSON.stringify(interleavingFields1);
-            const result = letterService.validateJson(JSON.parse(valJson), JSON.parse(testJson));
+            const result = jsonValidation.validateJson(JSON.parse(valJson), JSON.parse(testJson));
 
             expect(result.constructor === {}.constructor).to.be.true;
             expect(JSON.stringify(result)).to.be.equal(JSON.stringify(interleavingFields1));
         });
 
         it('should return an error when the input structure is not ok', () => {
-            return letterService.processJson('tem1', interleavingFields2)
+            return jsonValidation.processJson('tem1', interleavingFields2)
                 .catch((error) => {
                     expect(error).to.exist;
                     expect(error instanceof InterleavingFieldsError).to.be.true;
--- src/services/letterService.js
+++ src/services/letterService.js
@@ -1,11 +1,8 @@
 import handlebars from "handlebars";
-import fs from "mz/fs";
-import path from "path";
-import InterleavingFieldsError from "../utils/Exceptions/InterleavingFieldsError";
+import jsonValidation from "../utils/jsonValidation";
 import showdownOptions from "../utils/showdownOptions";
 import templateService from "./templateService";
 import showdown from 'showdown';
-import Ajv from 'ajv';
 import puppeteer from 'puppeteer';
 
 export default {
@@ -18,7 +15,7 @@ export default {
         }
 
         try {
-            return await this.processJson(templateName, interleavingFields, verify)
+            return await jsonValidation.processJson(templateName, interleavingFields, verify)
                 .then( (jsonData) => {
                     const hbs = handlebars.compile(markdownTemplate);
                     const compiledHbs = hbs(jsonData);
@@ -34,7 +31,7 @@ export default {
                             .catch((error) => {throw error});
                     }
                 })
-                .catch((error) => {throw error;});
+                .catch((error) => {throw error});
         }
         catch (error) {throw error;}
     },
@@ -59,53 +56,6 @@ export default {
             .then( (pdfa) => {
                 browser.close();
                 return pdfa;
-            });
-    },
-
-    async processJson(templateName, interleavingFields, verify = true){
-        interleavingFields = typeof interleavingFields !== "string"
-            ? JSON.stringify(interleavingFields) : interleavingFields;
-        const jsonData = JSON.parse(interleavingFields);
-
-        if(verify){
-            try {
-                return await this.findJsonSchema(templateName)
-                    .then((schema) => {
-                        const validationSchema = JSON.parse(schema);
-                        return this.validateJson(validationSchema, jsonData);
-                    })
-                    .catch((error) => {throw error;});
-            }
-            catch (error) {throw error;}
-        }
-        return jsonData;
-    },
-
-    validateJson(schema, jsonData){
-        const ajv = new Ajv({allErrors: true});
-        const validator = ajv.compile(schema);
-        const valid = validator(jsonData);
-        if(!valid){
-            throw new InterleavingFieldsError(validator.errors);
-        }
-        return jsonData;
-    },
-
-    getJsonSchemaPath(templateName){
-        const tempName = templateName.toLocaleLowerCase();
-        return path.join(__dirname + `/../templates/${tempName}/${tempName}.json`);
-    },
-
-    async findJsonSchema(templateName){
-        const path = this.getJsonSchemaPath(templateName);
-        return await fs.readFile(path, 'utf-8')
-            .then(data => {
-                return data;
-            })
-            .catch((err) => {
-                if(err.code === "ENOENT"){
-                    throw new Error("Could not find JSON schema to validate on.")
-                }
             });
     },
 }
